887abf443895a2fb4277476eff2b5940ec669e47
Taher027 <abutaherh27@gmail.com>
2022-10-28 11:02:51 +0600

    update course api

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ app.get("/", (req, res) => {
 })
 
 const courses = require('./data/courses.json');
+
 app.get("/courses", (req, res) =>{
     res.send(courses);
 })
 
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const getSingleCourse = courses?.find(c => c.id == id);
+    if(!getSingleCourse){
+        res.send('NO data found!')
+    }
+    res.send(getSingleCourse);
+})
 
 
 
